2d282ccee9f148a98298066f156cbbbf509a0506
Kamil Wdowicz <kwdowicz@gmail.com>
2024-05-10 18:04:11 +0200

    moved almost whole view

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 extern crate cursive_table_view;
 
-use cursive::{
-    align::HAlign,
-    event::Key,
-    views::{Button, Dialog, TextView},
-    CbSink, Cursive, CursiveRunnable,
-};
+use cursive::{event::Key, Cursive};
 use reqwest::blocking;
 use reqwest::Error;
 mod models;
-use cursive::traits::*;
-use cursive_table_view::{TableColumn, TableView, TableViewItem};
+use cursive_table_view::TableView;
 use models::container::{Container, Containers};
 mod views;
-use views::container::ContainerColumn;
+use views::container::{containers_view, ContainerColumn};
 
 fn fetch_containers() -> Result<Containers, Error> {
     let url = "http://localhost:2375/containers/json";
@@ -54,55 +48,8 @@ fn stop_container(container_id: &String) -> Result<(), Error> {
 
 fn main() {
     let mut siv = cursive::default();
-
-    let mut table = TableView::<Container, ContainerColumn>::new()
-        .column(ContainerColumn::Id, "Id", |c| c.width_percent(20))
-        .column(ContainerColumn::Image, "Image", |c| c.width_percent(20))
-        .column(ContainerColumn::Command, "Command", |c| {
-            c.align(HAlign::Left)
-        });
-
-    table.set_on_submit(move |siv: &mut Cursive, row: usize, index: usize| {
-        let value = siv
-            .call_on_name(
-                "containers_table",
-                move |table: &mut TableView<Container, ContainerColumn>| {
-                    format!("{:?}", table.borrow_item(index).unwrap())
-                },
-            )
-            .unwrap();
-
-        siv.add_layer(
-            Dialog::around(TextView::new(value))
-                .title(format!("Removing row # {}", row))
-                .button("Back", |s| {
-                    s.pop_layer();
-                })
-                .button("Stop", move |s| {
-                    s.call_on_name(
-                        "containers_table",
-                        |table: &mut TableView<Container, ContainerColumn>| {
-                            let item = table.borrow_item(index).unwrap();
-                            let container_id = item.id.clone();
-                            match stop_container(&container_id.unwrap()) {
-                                Ok(_) => {
-                                    table.remove_item(index);
-                                }
-                                Err(e) => eprintln!("Failed to stop container: {}", e),
-                            }
-                        },
-                    );
-                    s.pop_layer();
-                }),
-        );
-    });
-    siv.add_layer(
-        Dialog::around(table.with_name("containers_table").min_size((50, 20)))
-            .title("Containers")
-            .full_width(),
-    );
+    siv.add_layer(containers_view());
     update_containers_view(&mut siv);
-
     siv.add_global_callback(Key::F5, |s| update_containers_view(s));
     siv.run();
 }
--- src/views/container.rs
+++ src/views/container.rs
@@ -1,6 +1,8 @@
-use std::cmp::Ordering;
-use cursive_table_view::TableViewItem;
 use crate::models::container::Container;
+use cursive::{align::HAlign, views::{Dialog, ResizedView, TextView}, Cursive};
+use cursive_table_view::{TableView, TableViewItem};
+use std::cmp::Ordering;
+use cursive::traits::*;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub enum ContainerColumn {
@@ -23,9 +25,69 @@ impl TableViewItem<ContainerColumn> for Container {
         Self: Sized,
     {
         match column {
-            ContainerColumn::Id => self.id.as_ref().unwrap_or(&"NO_ID".to_string()).cmp(&other.id.as_ref().unwrap_or(&"NO_ID".to_string())),
-            ContainerColumn::Image => self.image.as_ref().unwrap_or(&"NO_IMAGE".to_string()).cmp(&other.image.as_ref().unwrap_or(&"NO_IMAGE".to_string())),
-            ContainerColumn::Command => self.command.as_ref().unwrap_or(&"NO_COMMAND".to_string()).cmp(&other.command.as_ref().unwrap_or(&"NO_COMMAND".to_string())),
+            ContainerColumn::Id => self
+                .id
+                .as_ref()
+                .unwrap_or(&"NO_ID".to_string())
+                .cmp(&other.id.as_ref().unwrap_or(&"NO_ID".to_string())),
+            ContainerColumn::Image => self
+                .image
+                .as_ref()
+                .unwrap_or(&"NO_IMAGE".to_string())
+                .cmp(&other.image.as_ref().unwrap_or(&"NO_IMAGE".to_string())),
+            ContainerColumn::Command => self
+                .command
+                .as_ref()
+                .unwrap_or(&"NO_COMMAND".to_string())
+                .cmp(&other.command.as_ref().unwrap_or(&"NO_COMMAND".to_string())),
         }
     }
+}
+
+pub fn containers_view() -> ResizedView<Dialog> {
+    let mut table = TableView::<Container, ContainerColumn>::new()
+        .column(ContainerColumn::Id, "Id", |c| c.width_percent(20))
+        .column(ContainerColumn::Image, "Image", |c| c.width_percent(20))
+        .column(ContainerColumn::Command, "Command", |c| {
+            c.align(HAlign::Left)
+        });
+
+    table.set_on_submit(move |siv: &mut Cursive, row: usize, index: usize| {
+        let value = siv
+            .call_on_name(
+                "containers_table",
+                move |table: &mut TableView<Container, ContainerColumn>| {
+                    format!("{:?}", table.borrow_item(index).unwrap())
+                },
+            )
+            .unwrap();
+
+        siv.add_layer(
+            Dialog::around(TextView::new(value))
+                .title(format!("Removing row # {}", row))
+                .button("Back", |s| {
+                    s.pop_layer();
+                })
+                .button("Stop", move |s| {
+                    s.call_on_name(
+                        "containers_table",
+                        |table: &mut TableView<Container, ContainerColumn>| {
+                            let item = table.borrow_item(index).unwrap();
+                            let container_id = item.id.clone();
+                            // match stop_container(&container_id.unwrap()) {
+                            //     Ok(_) => {
+                            //         table.remove_item(index);
+                            //     }
+                            //     Err(e) => eprintln!("Failed to stop container: {}", e),
+                            // }
+                        },
+                    );
+                    s.pop_layer();
+                }),
+        );
+    });
+
+    Dialog::around(table.with_name("containers_table").min_size((50, 20)))
+            .title("Containers")
+            .full_width()
 }
