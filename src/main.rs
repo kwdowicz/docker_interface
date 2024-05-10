2ea715ac851aa95677e12287f652003614b2808c
Kamil Wdowicz <kwdowicz@gmail.com>
2024-05-10 17:53:52 +0200

    before files reorg

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 extern crate cursive_table_view;
 
+use cursive::{
+    align::HAlign,
+    event::Key,
+    views::{Button, Dialog, TextView},
+    CbSink, Cursive, CursiveRunnable,
+};
 use reqwest::blocking;
-use cursive::{align::HAlign, event::Key, views::{Dialog, TextView}, CbSink, Cursive, CursiveRunnable};
 use reqwest::Error;
 mod models;
-use models::container::{Containers, Container};
-use cursive_table_view::{TableViewItem, TableColumn, TableView};
 use cursive::traits::*;
+use cursive_table_view::{TableColumn, TableView, TableViewItem};
+use models::container::{Container, Containers};
 mod views;
 use views::container::ContainerColumn;
 
@@ -25,8 +30,11 @@ fn update_containers_view(siv: &mut Cursive) {
             for container in containers {
                 container_rows.push(container);
             }
-            siv.call_on_name("containers_table", |table: &mut TableView<Container, ContainerColumn>| table.set_items(container_rows));
-        },
+            siv.call_on_name(
+                "containers_table",
+                |table: &mut TableView<Container, ContainerColumn>| table.set_items(container_rows),
+            );
+        }
         Err(e) => eprintln!("Failed to fetch containers: {}", e),
     }
 }
@@ -41,7 +49,6 @@ fn stop_container(container_id: &String) -> Result<(), Error> {
     } else {
         println!("Failed to stop container: Status {}", res.status());
     }
-
     Ok(())
 }
 
@@ -51,42 +58,51 @@ fn main() {
     let mut table = TableView::<Container, ContainerColumn>::new()
         .column(ContainerColumn::Id, "Id", |c| c.width_percent(20))
         .column(ContainerColumn::Image, "Image", |c| c.width_percent(20))
-        .column(ContainerColumn::Command, "Command", |c| c.align(HAlign::Left));
+        .column(ContainerColumn::Command, "Command", |c| {
+            c.align(HAlign::Left)
+        });
 
     table.set_on_submit(move |siv: &mut Cursive, row: usize, index: usize| {
         let value = siv
-            .call_on_name("containers_table", move |table: &mut TableView<Container, ContainerColumn>| {
-                format!("{:?}", table.borrow_item(index).unwrap())
-            })
+            .call_on_name(
+                "containers_table",
+                move |table: &mut TableView<Container, ContainerColumn>| {
+                    format!("{:?}", table.borrow_item(index).unwrap())
+                },
+            )
             .unwrap();
-
-        println!("{:?}", value);
 
         siv.add_layer(
             Dialog::around(TextView::new(value))
                 .title(format!("Removing row # {}", row))
-                .button("Stop", move |s| {
-                    s.call_on_name("containers_table", |table: &mut TableView<Container, ContainerColumn>| {
-                        let item = table.borrow_item(index).unwrap();
-                        let container_id = item.id.clone();
-                        match stop_container(&container_id.unwrap()) {
-                            Ok(_) => {
-                                table.remove_item(index);
-                            },
-                            Err(e) => eprintln!("Failed to stop container: {}", e),
-                        }
-                    });
+                .button("Back", |s| {
                     s.pop_layer();
-                }
-            ),
+                })
+                .button("Stop", move |s| {
+                    s.call_on_name(
+                        "containers_table",
+                        |table: &mut TableView<Container, ContainerColumn>| {
+                            let item = table.borrow_item(index).unwrap();
+                            let container_id = item.id.clone();
+                            match stop_container(&container_id.unwrap()) {
+                                Ok(_) => {
+                                    table.remove_item(index);
+                                }
+                                Err(e) => eprintln!("Failed to stop container: {}", e),
+                            }
+                        },
+                    );
+                    s.pop_layer();
+                }),
         );
     });
-    siv.add_layer(Dialog::around(table.with_name("containers_table").min_size((50, 20))).title("Containers").full_width());
+    siv.add_layer(
+        Dialog::around(table.with_name("containers_table").min_size((50, 20)))
+            .title("Containers")
+            .full_width(),
+    );
     update_containers_view(&mut siv);
-    
-    siv.add_global_callback(Key::F5, |s| {
-        update_containers_view(s)
-    });
+
+    siv.add_global_callback(Key::F5, |s| update_containers_view(s));
     siv.run();
 }
-
